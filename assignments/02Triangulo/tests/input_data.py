c75e9629af0bd756e36502870ae8e7edee84df05
Ricardo Quintero <rquintero@itculiacan.edu.mx>
2021-08-09 18:48:36 +0000

    Listo ejercicio Triangulo

---
+++ assignments/02Triangulo/tests/input_data.py
@@ -0,0 +1,15 @@
+# List of tuples
+# Each tuple contains a test:
+# - the first element are the inputs,
+# - the second are the output,
+# - and the third is the message in case of an error
+# To test another case, add another tuple
+
+input_values = [
+    # Test case 1
+    (
+        ["7","12"],
+        ["AREA DE UN TRIANGULO","====================","Dame la base:","Dame la altura:","Area=42.0"],
+        "Revisa los requisitos del ejercicio, entrada por entrada, salida por salida"
+    )
+]
